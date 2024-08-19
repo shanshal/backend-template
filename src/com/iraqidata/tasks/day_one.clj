4cd42e398971e725c44d84094381ccee6eadfc90
Ahmed Mahdi Shanshal <ahmedmahdii2003@gmail.com>
2024-08-19 11:45:41 +0300

    Ahmed Mahdi Shanshal Day 1 Submission
    I have not run these yet i literally did them on my phone in college

--- src/com/iraqidata/tasks/day_one.clj
+++ src/com/iraqidata/tasks/day_one.clj
@@ -3,25 +3,40 @@
 
 ;; 1. Write a function that takes one argument as input and prints that
 ;; argument.
+(defn customPrint [text]
+  (println text))
 
-
-
-
+;; for usage (customPrint [your text here]
 
 
 ;; 2. Write a function that adds `1` to a number only if the input is odd,
 ;; otherwise return `:error`.
+(defn makeEven [number])
+(
+ if (odd? number)
+ number + 1)
+
+(defn makeEven [number]
+  (if (odd? number)
+    (+ number 1)
+    number))
+
+
+;;usage (makeEven 3)
 
 ;; Tip, use the `odd?` function to check if a number is odd.
 
 
-
-
-
-
-
 ;; 3. Write a function that takes 3 arguments, `name`, `year-of-birth`, and
 ;; `current-year`. and returns a map with the following keys: `name`, `age`.
+
+(defn find-age [{:keys [name year-of-birth current-year]}] 
+  {:name name
+   :age (- current-year year-of-birth)}
+  )
+
+
+
 
 ;; Example run
 ;; (function-name "Ali" 2001 2024) => {:name "Ali", :age 23}
@@ -29,9 +44,12 @@
 
 
 
-
 ;; 4. Write a function that takes the output of the above function and returns
 ;; `true` if the person is allowed to vote (assume the voting age is 18).
+
+(defn is-adult [find-age]
+  (>= (:age find-age) 18))
+
 
 ;; Example run
 ;; (function-name {:name "Ali", :age 23}) => true
@@ -44,6 +62,15 @@
 ;; OPTIONAL FOR BONUS POINTS
 
 ;; 5. Modify the function from number 3 to not need the `current-year`.
+
+(import '[java.time LocalDate])
+
+(defn sentiant-find-age [{:keys [name year-of-birth]}]
+  (let [current-year (.getYear (LocalDate/now))]
+    {:name name
+     :age (- current-year year-of-birth)}))
+
+
 ;; Example run
 ;; (function-name "Ali" 2001) => {:name "Ali", :age 23}
 ;; If ran in 2025
