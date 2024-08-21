af154f4ecbbd2ef338dd3870a3cd7e0c511d0d4d
adham-omran-ii <adham.a@kapita.iq>
2024-08-21 13:41:08 +0300

    Apply grading

--- src/com/iraqidata/tasks/day_2.clj
+++ src/com/iraqidata/tasks/day_2.clj
@@ -12,37 +12,50 @@
 (def total-flights
   (tc/row-count ds))
 
+total-flights
+;; => 336776
+
+;; 1 point
 
 ;; 2. How many unique carriers were there in total?
 (def unique-carriers
   (-> ds
-      (tc/select :carrier)
-      (tc/distinct)
+      (tc/select-columns :carrier)
+      (tc/unique-by :carrier)
       (tc/row-count)))
 
+unique-carriers
+;; => 16
+
+;;
 
 ;; 3. How many unique airports were there in total?
 (def airports
   (-> ds
       (tc/select-columns [:origin :dest])
-      (tc/distinct)
+      (tc/unique-by [:dest])
       (tc/row-count)))
 
+;; 0 point
 
 ;; 4. What is the average arrival delay for each month?
 (def avg-arr-delay
   (-> ds
       (tc/group-by :month)
-      (tc/aggregate {:avg-arr-delay (fn [col] (tc/mean col :arr-delay))})))
+      (tc/aggregate {:avg-arr-delay
+                     (fn [col]
+                       (tc/mean col :arr-delay))})))
 
+;; 1 point
 
 ;; Optional: Use the `airlines` dataset to get the name of the carrier with the
 ;; highest average distance.
+
 (def top-distance
   (-> ds
       (tc/group-by :carrier)
       (tc/aggregate {:avg-distance (fn [col] (tc/mean col :distance))})
-      (tc/sort-by :avg-distance :desc)
+      (tc/order-by :avg-distance-summary :desc)
       (tc/first)))
 
 (def airlines
