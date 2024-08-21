95530bcd69f716de1955a44b7948319c4d0786a5
Ahmed Mahdi Shanshal <ahmedmahdii2003@gmail.com>
2024-08-21 12:29:52 +0300

    Ahmed Mahdi Shanshal Day 2 Submission

--- src/com/iraqidata/tasks/day_2.clj
+++ src/com/iraqidata/tasks/day_2.clj
@@ -9,19 +9,41 @@
                     {:key-fn #(keyword (str/replace (name %) "_" "-"))}))
 
 ;; 1. How many flights were there in total?
+(def total-flights
+  (tc/row-count ds))
 
 
 ;; 2. How many unique carriers were there in total?
+(def unique-carriers
+  (-> ds
+      (tc/select :carrier)
+      (tc/distinct)
+      (tc/row-count)))
 
 
 ;; 3. How many unique airports were there in total?
+(def airports
+  (-> ds
+      (tc/select-columns [:origin :dest])
+      (tc/distinct)
+      (tc/row-count)))
 
 
 ;; 4. What is the average arrival delay for each month?
+(def avg-arr-delay
+  (-> ds
+      (tc/group-by :month)
+      (tc/aggregate {:avg-arr-delay (fn [col] (tc/mean col :arr-delay))})))
 
 
 ;; Optional: Use the `airlines` dataset to get the name of the carrier with the
 ;; highest average distance.
+(def top-distance
+  (-> ds
+      (tc/group-by :carrier)
+      (tc/aggregate {:avg-distance (fn [col] (tc/mean col :distance))})
+      (tc/sort-by :avg-distance :desc)
+      (tc/first)))
 
 (def airlines
   (tc/dataset "./resources/data/airlines.csv"
